ab38ceea2ba9a0733542034d6d70c5668107d610
Filip Szewczyk <filip.szewczyk@gmail.com>
2021-09-10 00:10:38 +0200

    Added support for conditional expressions

--- Lindhart.Analyser.MissingAwaitWarning/Lindhart.Analyser.MissingAwaitWarning/Lindhart.Analyser.MissingAwaitWarning/LindhartAnalyserMissingAwaitWarningAnalyzer.cs
+++ Lindhart.Analyser.MissingAwaitWarning/Lindhart.Analyser.MissingAwaitWarning/Lindhart.Analyser.MissingAwaitWarning/LindhartAnalyserMissingAwaitWarningAnalyzer.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Lindhart.Analyser.MissingAwaitWarning
 {
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [DiagnosticAnalyzer( LanguageNames.CSharp )]
     public class LindhartAnalyserMissingAwaitWarningAnalyzer : DiagnosticAnalyzer
     {
         public const string StandardRuleId = "LindhartAnalyserMissingAwaitWarning";
@@ -17,14 +17,14 @@ namespace Lindhart.Analyser.MissingAwaitWarning
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString StandardTitle = new LocalizableResourceString(nameof(Resources.StandardRuleTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString StrictTitle = new LocalizableResourceString(nameof(Resources.StandardRuleTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString StandardTitle = new LocalizableResourceString( nameof( Resources.StandardRuleTitle ), Resources.ResourceManager, typeof( Resources ) );
+        private static readonly LocalizableString StrictTitle = new LocalizableResourceString( nameof( Resources.StandardRuleTitle ), Resources.ResourceManager, typeof( Resources ) );
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString( nameof( Resources.AnalyzerMessageFormat ), Resources.ResourceManager, typeof( Resources ) );
+        private static readonly LocalizableString Description = new LocalizableResourceString( nameof( Resources.AnalyzerDescription ), Resources.ResourceManager, typeof( Resources ) );
         private const string Category = "UnintentionalUsage";
 
-        private static readonly DiagnosticDescriptor StandardRule = new DiagnosticDescriptor(StandardRuleId, StandardTitle, MessageFormat, Category, DiagnosticSeverity.Warning, true, Description);
-        private static readonly DiagnosticDescriptor StrictRule = new DiagnosticDescriptor(StrictRuleId, StrictTitle, MessageFormat, Category, DiagnosticSeverity.Hidden, false, Description);
+        private static readonly DiagnosticDescriptor StandardRule = new DiagnosticDescriptor( StandardRuleId, StandardTitle, MessageFormat, Category, DiagnosticSeverity.Warning, true, Description );
+        private static readonly DiagnosticDescriptor StrictRule = new DiagnosticDescriptor( StrictRuleId, StrictTitle, MessageFormat, Category, DiagnosticSeverity.Hidden, false, Description );
 
         private static readonly string[] AwaitableTypes = new[]
         {
@@ -38,53 +38,71 @@ namespace Lindhart.Analyser.MissingAwaitWarning
             typeof(ConfiguredValueTaskAwaitable<>).FullName
         };
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(StandardRule, StrictRule);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create( StandardRule, StrictRule );
 
-        public override void Initialize(AnalysisContext context)
+        public override void Initialize( AnalysisContext context )
         {
-            context.RegisterSyntaxNodeAction(AnalyseSymbolNode, SyntaxKind.InvocationExpression);
+            context.RegisterSyntaxNodeAction( AnalyseSymbolNode, SyntaxKind.InvocationExpression );
         }
 
-        private void AnalyseSymbolNode(SyntaxNodeAnalysisContext syntaxNodeAnalysisContext)
+        private void AnalyseSymbolNode( SyntaxNodeAnalysisContext syntaxNodeAnalysisContext )
         {
-            if (syntaxNodeAnalysisContext.Node is InvocationExpressionSyntax node)
+            if ( syntaxNodeAnalysisContext.Node is InvocationExpressionSyntax node )
             {
                 var symbolInfo = syntaxNodeAnalysisContext
                     .SemanticModel
-                    .GetSymbolInfo(node.Expression, syntaxNodeAnalysisContext.CancellationToken);
+                    .GetSymbolInfo( node.Expression, syntaxNodeAnalysisContext.CancellationToken );
 
-                if ((symbolInfo.Symbol ?? symbolInfo.CandidateSymbols.FirstOrDefault())
-                    is IMethodSymbol methodSymbol)
+                if ( ( symbolInfo.Symbol ?? symbolInfo.CandidateSymbols.FirstOrDefault() )
+                    is IMethodSymbol methodSymbol )
                 {
-                    switch (node.Parent)
-                    {
-                        // Checks if a task is not awaited when the task itself is not assigned to a variable.
-                        case ExpressionStatementSyntax _:
-                            // Check the method return type against all the known awaitable types.
-                            if (EqualsType(methodSymbol.ReturnType, syntaxNodeAnalysisContext.SemanticModel, AwaitableTypes))
-                            {
-                                var diagnostic = Diagnostic.Create(StandardRule, node.GetLocation(), methodSymbol.ToDisplayString());
-
-                                syntaxNodeAnalysisContext.ReportDiagnostic(diagnostic);
-                            }
-
-                            break;
-
-                        // Checks if a task is not awaited in lambdas.
-                        case AnonymousFunctionExpressionSyntax _:
-                        case ArrowExpressionClauseSyntax _:
-                        // Checks if a task is not awaited when the task itself is assigned to a variable.
-                        case EqualsValueClauseSyntax _:
-                            if (EqualsType(methodSymbol.ReturnType, syntaxNodeAnalysisContext.SemanticModel, AwaitableTypes))
-                            {
-                                var diagnostic = Diagnostic.Create(StrictRule, node.GetLocation(), methodSymbol.ToDisplayString());
-
-                                syntaxNodeAnalysisContext.ReportDiagnostic(diagnostic);
-                            }
-
-                            break;
-                    }
+                    AnalyseParentNode( syntaxNodeAnalysisContext, node, methodSymbol );
                 }
+            }
+        }
+
+        private static void AnalyseParentNode( SyntaxNodeAnalysisContext syntaxNodeAnalysisContext, SyntaxNode node, IMethodSymbol methodSymbol )
+        {
+            switch ( node.Parent )
+            {
+                // Checks if a task is not awaited when the task itself is not assigned to a variable.
+                case ExpressionStatementSyntax _:
+                    // Check the method return type against all the known awaitable types.
+                    if ( EqualsType( methodSymbol.ReturnType, syntaxNodeAnalysisContext.SemanticModel, AwaitableTypes ) )
+                    {
+                        var diagnostic = Diagnostic.Create( StandardRule, node.GetLocation(), methodSymbol.ToDisplayString() );
+
+                        syntaxNodeAnalysisContext.ReportDiagnostic( diagnostic );
+                    }
+
+                    break;
+
+                // Checks if a task is not awaited in lambdas.
+                case AnonymousFunctionExpressionSyntax _:
+                case ArrowExpressionClauseSyntax _:
+                // Checks if a task is not awaited when the task itself is assigned to a variable.
+                case EqualsValueClauseSyntax _:
+
+                    if ( EqualsType( methodSymbol.ReturnType, syntaxNodeAnalysisContext.SemanticModel, AwaitableTypes ) )
+                    {
+                        var diagnostic = Diagnostic.Create( StrictRule, node.GetLocation(), methodSymbol.ToDisplayString() );
+
+                        syntaxNodeAnalysisContext.ReportDiagnostic( diagnostic );
+                    }
+
+                    break;
+
+                // If the conditional expression, we check recursively
+                case ConditionalAccessExpressionSyntax _:
+
+                    AnalyseParentNode( syntaxNodeAnalysisContext, node.Parent, methodSymbol );
+
+                    break;
+
+                // Awaited expressions don't interest us
+                case AwaitExpressionSyntax _:
+
+                    break;
             }
         }
 
@@ -95,18 +113,18 @@ namespace Lindhart.Analyser.MissingAwaitWarning
         /// <param name="semanticModel">Semantic Model of the current context</param>
         /// <param name="types">List of parameters that should match the symbol's type</param>
         /// <returns></returns>
-        private static bool EqualsType(ITypeSymbol typeSymbol, SemanticModel semanticModel, params string[] types)
+        private static bool EqualsType( ITypeSymbol typeSymbol, SemanticModel semanticModel, params string[] types )
         {
-            var namedTypeSymbols = types.Select(x => semanticModel.Compilation.GetTypeByMetadataName(x));
+            var namedTypeSymbols = types.Select( x => semanticModel.Compilation.GetTypeByMetadataName( x ) );
 
             var namedSymbol = typeSymbol as INamedTypeSymbol;
-            if (namedSymbol == null)
+            if ( namedSymbol == null )
                 return false;
 
-            if (namedSymbol.IsGenericType)
-                return namedTypeSymbols.Any(t => namedSymbol.ConstructedFrom.Equals(t));
-            
-            return namedTypeSymbols.Any(t => typeSymbol.Equals(t));
+            if ( namedSymbol.IsGenericType )
+                return namedTypeSymbols.Any( t => namedSymbol.ConstructedFrom.Equals( t ) );
+
+            return namedTypeSymbols.Any( t => typeSymbol.Equals( t ) );
         }
     }
 }
